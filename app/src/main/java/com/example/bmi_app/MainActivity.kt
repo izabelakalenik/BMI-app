677c88d04887f07598dd0e6bc6e785bccde3230a
Iza <iza.kalenik@gmail.com>
2023-11-07 14:12:21 +0100

    added: colored text, many descriptions of bmi values, another activity

--- app/src/main/java/com/example/bmi_app/MainActivity.kt
+++ app/src/main/java/com/example/bmi_app/MainActivity.kt
@@ -7,6 +7,7 @@ import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 
 
 class MainActivity : AppCompatActivity() {
@@ -28,12 +29,30 @@ class MainActivity : AppCompatActivity() {
             calculateBMI()
         }
 
-        val send = Intent(this,
-            ResultActivity::class.java
-        )
+        resultBMI.setOnClickListener {
+            ahh()
+        }
 
-        startActivity(send)
     }
+
+//    private fun calculateBMI() {
+//        val heightStr = inputH.text.toString()
+//        val weightStr = inputW.text.toString()
+//
+//        if (heightStr.isNotEmpty() && weightStr.isNotEmpty()) {
+//            val height = heightStr.toDouble() / 100
+//            val weight = weightStr.toDouble()
+//
+//            val bmi = weight / (height * height)
+//
+//            val formattedBMI = String.format("%.2f", bmi)
+//
+//            val resultText = getString(R.string.bmi_result, formattedBMI)
+//            resultBMI.text = resultText
+//        } else {
+//            resultBMI.text = getString(R.string.input_error)
+//        }
+//    }
 
     private fun calculateBMI() {
         val heightStr = inputH.text.toString()
@@ -47,11 +66,56 @@ class MainActivity : AppCompatActivity() {
 
             val formattedBMI = String.format("%.2f", bmi)
 
-            val resultText = getString(R.string.bmi_result, formattedBMI)
-            resultBMI.text = resultText
+
+
+            val resultText = when {
+                bmi < 16.0 -> {
+                    resultBMI.setTextColor(ContextCompat.getColor(this, R.color.red))
+                    "Underweight"
+                }
+                bmi in 16.0..16.99 -> {
+                    resultBMI.setTextColor(ContextCompat.getColor(this, R.color.orange))
+                    "Severe Thinness"
+                }
+                bmi in 17.0..18.49 -> {
+                    resultBMI.setTextColor(ContextCompat.getColor(this, R.color.yellow))
+                    "Mild Thinness"
+                }
+                bmi in 18.5..24.99 -> {
+                    resultBMI.setTextColor(ContextCompat.getColor(this, R.color.green))
+                    "Normal Weight"
+                }
+                bmi in 25.0..29.9 -> {
+                    resultBMI.setTextColor(ContextCompat.getColor(this, R.color.orange))
+                    "Overweight"
+                }
+                bmi in (30.0..34.99) -> {
+                    resultBMI.setTextColor(ContextCompat.getColor(this, R.color.red))
+                    "Obesity Class I"
+                }
+                bmi in (35.0..39.99) -> {
+                    resultBMI.setTextColor(ContextCompat.getColor(this, R.color.red))
+                    "Obesity Class II"
+                }
+                else -> {
+                    resultBMI.setTextColor(ContextCompat.getColor(this, R.color.red))
+                    "Obesity Class III (Morbidly Obese)"
+                }
+            }
+
+            resultBMI.text = getString(R.string.bmi_result, formattedBMI)
+            resultBMI.append("\n$resultText")
         } else {
             resultBMI.text = getString(R.string.input_error)
         }
+    }
+
+
+    private fun ahh(){
+        val result = Intent(this,
+            ResultActivity::class.java)
+
+        startActivity(result)
     }
 
 }
