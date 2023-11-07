bdea4c6891c07dcabaa6b71a8e0a76cced0eee34
Iza <iza.kalenik@gmail.com>
2023-11-07 20:50:29 +0100

    extracted to functions

--- app/src/main/java/com/example/bmi_app/MainActivity.kt
+++ app/src/main/java/com/example/bmi_app/MainActivity.kt
@@ -20,102 +20,100 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        createVars()
+
+        buttonBMI.setOnClickListener {
+            runBMI()
+        }
+
+        resultBMI.setOnClickListener {
+            openResultActivity()
+        }
+
+    }
+
+    private fun createVars(){
         inputH = findViewById(R.id.inputH)
         inputW = findViewById(R.id.inputW)
         resultBMI = findViewById(R.id.resultBMI)
         buttonBMI = findViewById(R.id.buttonBMI)
-
-        buttonBMI.setOnClickListener {
-            calculateBMI()
-        }
-
-        resultBMI.setOnClickListener {
-            ahh()
-        }
-
     }
 
-//    private fun calculateBMI() {
-//        val heightStr = inputH.text.toString()
-//        val weightStr = inputW.text.toString()
-//
-//        if (heightStr.isNotEmpty() && weightStr.isNotEmpty()) {
-//            val height = heightStr.toDouble() / 100
-//            val weight = weightStr.toDouble()
-//
-//            val bmi = weight / (height * height)
-//
-//            val formattedBMI = String.format("%.2f", bmi)
-//
-//            val resultText = getString(R.string.bmi_result, formattedBMI)
-//            resultBMI.text = resultText
-//        } else {
-//            resultBMI.text = getString(R.string.input_error)
-//        }
-//    }
-
-    private fun calculateBMI() {
+    private fun runBMI(){
         val heightStr = inputH.text.toString()
         val weightStr = inputW.text.toString()
 
         if (heightStr.isNotEmpty() && weightStr.isNotEmpty()) {
-            val height = heightStr.toDouble() / 100
-            val weight = weightStr.toDouble()
 
-            val bmi = weight / (height * height)
-
-            val formattedBMI = String.format("%.2f", bmi)
-
-
-
-            val resultText = when {
-                bmi < 16.0 -> {
-                    resultBMI.setTextColor(ContextCompat.getColor(this, R.color.red))
-                    "Underweight"
-                }
-                bmi in 16.0..16.99 -> {
-                    resultBMI.setTextColor(ContextCompat.getColor(this, R.color.orange))
-                    "Severe Thinness"
-                }
-                bmi in 17.0..18.49 -> {
-                    resultBMI.setTextColor(ContextCompat.getColor(this, R.color.yellow))
-                    "Mild Thinness"
-                }
-                bmi in 18.5..24.99 -> {
-                    resultBMI.setTextColor(ContextCompat.getColor(this, R.color.green))
-                    "Normal Weight"
-                }
-                bmi in 25.0..29.9 -> {
-                    resultBMI.setTextColor(ContextCompat.getColor(this, R.color.orange))
-                    "Overweight"
-                }
-                bmi in (30.0..34.99) -> {
-                    resultBMI.setTextColor(ContextCompat.getColor(this, R.color.red))
-                    "Obesity Class I"
-                }
-                bmi in (35.0..39.99) -> {
-                    resultBMI.setTextColor(ContextCompat.getColor(this, R.color.red))
-                    "Obesity Class II"
-                }
-                else -> {
-                    resultBMI.setTextColor(ContextCompat.getColor(this, R.color.red))
-                    "Obesity Class III (Morbidly Obese)"
-                }
-            }
-
-            resultBMI.text = getString(R.string.bmi_result, formattedBMI)
-            resultBMI.append("\n$resultText")
-        } else {
+           val calculatedBMI = calculateBMI(heightStr.toDouble(), weightStr.toDouble())
+           displayBMI(calculatedBMI)
+        }
+        else {
             resultBMI.text = getString(R.string.input_error)
         }
+
+      }
+
+    private fun calculateBMI(heightInCm: Double, weightInKg: Double) : Double {
+        val heightInMeters = heightInCm / 100
+        return weightInKg / (heightInMeters * heightInMeters)
     }
 
+    private fun displayBMI(bmi: Double){
 
-    private fun ahh(){
-        val result = Intent(this,
+        val resultText = getResultColor(bmi)
+        val formattedBMI = String.format("%.2f", bmi)
+
+        resultBMI.text = getString(R.string.bmi_result, formattedBMI)
+        resultBMI.append("\n$resultText")
+
+    }
+
+    private fun getResultColor(bmi: Double) : String{
+
+        val resultText = when {
+            bmi < 16.0 -> {
+                resultBMI.setTextColor(ContextCompat.getColor(this, R.color.red))
+                "Underweight"
+            }
+            bmi in 16.0..16.99 -> {
+                resultBMI.setTextColor(ContextCompat.getColor(this, R.color.orange))
+                "Severe Thinness"
+            }
+            bmi in 17.0..18.49 -> {
+                resultBMI.setTextColor(ContextCompat.getColor(this, R.color.yellow))
+                "Mild Thinness"
+            }
+            bmi in 18.5..24.99 -> {
+                resultBMI.setTextColor(ContextCompat.getColor(this, R.color.green))
+                "Normal Weight"
+            }
+            bmi in 25.0..29.9 -> {
+                resultBMI.setTextColor(ContextCompat.getColor(this, R.color.orange))
+                "Overweight"
+            }
+            bmi in (30.0..34.99) -> {
+                resultBMI.setTextColor(ContextCompat.getColor(this, R.color.red))
+                "Obesity Class I"
+            }
+            bmi in (35.0..39.99) -> {
+                resultBMI.setTextColor(ContextCompat.getColor(this, R.color.red))
+                "Obesity Class II"
+            }
+            else -> {
+                resultBMI.setTextColor(ContextCompat.getColor(this, R.color.red))
+                "Obesity Class III (Morbidly Obese)"
+            }
+        }
+        return resultText
+    }
+
+    private fun openResultActivity(){
+        val resultActivity = Intent(this,
             ResultActivity::class.java)
 
-        startActivity(result)
+        startActivity(resultActivity)
     }
 
 }
