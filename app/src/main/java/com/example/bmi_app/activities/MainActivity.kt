a64eaf8b2b76af6e7b0d08a445889ab1ba0489cb
Iza <iza.kalenik@gmail.com>
2023-11-08 16:40:26 +0100

    created getColor and getText

--- app/src/main/java/com/example/bmi_app/activities/MainActivity.kt
+++ app/src/main/java/com/example/bmi_app/activities/MainActivity.kt
@@ -16,15 +16,21 @@ import com.example.bmi_app.units.Imperial
 import com.example.bmi_app.units.Metric
 
 
+
 class MainActivity : AppCompatActivity() {
 
     private lateinit var labelH: TextView
     private lateinit var labelW: TextView
+
     private lateinit var inputH: EditText
     private lateinit var inputW: EditText
+
     private lateinit var resultBMI: TextView
+    private lateinit var resultTextBMI: TextView
+
     private lateinit var buttonBMI: Button
     private lateinit var unitSpinner: Spinner
+
     private var isMetricUnits = true
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -48,15 +54,19 @@ class MainActivity : AppCompatActivity() {
     private fun createVars(){
         labelH = findViewById(R.id.labelH)
         labelW = findViewById(R.id.labelW)
+
         inputH = findViewById(R.id.inputH)
         inputW = findViewById(R.id.inputW)
+
         resultBMI = findViewById(R.id.resultBMI)
+        resultTextBMI = findViewById(R.id.resultTextBMI)
+
         buttonBMI = findViewById(R.id.buttonBMI)
         unitSpinner = findViewById(R.id.unitSpinner)
 
     }
 
-   private fun createOnItemSelectedListener(): AdapterView.OnItemSelectedListener {
+    private fun createOnItemSelectedListener(): AdapterView.OnItemSelectedListener {
         return object : AdapterView.OnItemSelectedListener {
             override fun onItemSelected(
                 parent: AdapterView<*>?,
@@ -64,20 +74,29 @@ class MainActivity : AppCompatActivity() {
                 position: Int,
                 id: Long
             ) {
-                isMetricUnits =
-                    position == 0 // Jeśli wybrano "Metric", użyj jednostek metrycznych, w przeciwnym razie jednostek imperialnych
-                labelH.text = getString(R.string.height_imperial_label)
-                labelW.text = getString(R.string.weight_imperial_label)
-                // Wyczyść pola wprowadzania
+                isMetricUnits = position == 0
+
+                if (isMetricUnits) {
+                    // Set the labels to Metric labels
+                    labelH.text = getString(R.string.height_metric_label)
+                    labelW.text = getString(R.string.weight_metric_label)
+                } else {
+                    // Set the labels to Imperial labels
+                    labelH.text = getString(R.string.height_imperial_label)
+                    labelW.text = getString(R.string.weight_imperial_label)
+                }
+
+                // Clear the input fields
                 inputH.text.clear()
                 inputW.text.clear()
             }
 
             override fun onNothingSelected(parent: AdapterView<*>?) {
-                // Nie rób nic
+                // Do nothing
             }
         }
     }
+
 
     private fun runBMI(){
         val heightStr = inputH.text.toString()
@@ -106,58 +125,60 @@ class MainActivity : AppCompatActivity() {
 
     private fun displayBMI(bmi: Double){
 
-        val resultText = getResultColor(bmi)
+        val color = getColor(bmi)
+        val resultText = getResultText(bmi)
+
         val formattedBMI = String.format("%.2f", bmi)
 
-        resultBMI.text = getString(R.string.bmi_result, formattedBMI)
-        resultBMI.append("\n$resultText")
+        resultBMI.setTextColor(color)
+        resultBMI.text = formattedBMI
+
+        resultTextBMI.setTextColor(color)
+        resultTextBMI.text = resultText
 
     }
 
-    private fun getResultColor(bmi: Double) : String{
-
+    private fun getResultText(bmi: Double) : String{
         val resultText = when {
-            bmi < 16.0 -> {
-                resultBMI.setTextColor(ContextCompat.getColor(this, R.color.red))
-                "Underweight"
-            }
-            bmi in 16.0..16.99 -> {
-                resultBMI.setTextColor(ContextCompat.getColor(this, R.color.orange))
-                "Severe Thinness"
-            }
-            bmi in 17.0..18.49 -> {
-                resultBMI.setTextColor(ContextCompat.getColor(this, R.color.yellow))
-                "Mild Thinness"
-            }
-            bmi in 18.5..24.99 -> {
-                resultBMI.setTextColor(ContextCompat.getColor(this, R.color.green))
-                "Normal Weight"
-            }
-            bmi in 25.0..29.9 -> {
-                resultBMI.setTextColor(ContextCompat.getColor(this, R.color.orange))
-                "Overweight"
-            }
-            bmi in (30.0..34.99) -> {
-                resultBMI.setTextColor(ContextCompat.getColor(this, R.color.red))
-                "Obesity Class I"
-            }
-            bmi in (35.0..39.99) -> {
-                resultBMI.setTextColor(ContextCompat.getColor(this, R.color.red))
-                "Obesity Class II"
-            }
-            else -> {
-                resultBMI.setTextColor(ContextCompat.getColor(this, R.color.red))
-                "Obesity Class III (Morbidly Obese)"
-            }
+            bmi < 16.0 -> "Underweight"
+            bmi in (16.0..16.99) -> "Severe Thinness"
+            bmi in (17.0..18.49) -> "Mild Thinness"
+            bmi in (18.5..24.99) -> "Normal Weight"
+            bmi in (25.0..29.9) -> "Overweight"
+            bmi in (30.0..34.99) -> "Obesity Class I"
+            bmi in (35.0..39.99) -> "Obesity Class II"
+            else -> "Obesity Class III (Morbidly Obese)"
         }
         return resultText
+    }
+    private fun getColor(bmi: Double) : Int {
+
+        val color = when {
+            bmi < 16.0 -> ContextCompat.getColor(this, R.color.red)
+            bmi in (16.0..16.99) ->
+                ContextCompat.getColor(this, R.color.orange)
+            bmi in (17.0..18.49) ->
+                ContextCompat.getColor(this, R.color.yellow)
+            bmi in (18.5..24.99) ->
+                ContextCompat.getColor(this, R.color.green)
+            bmi in (25.0..29.9) ->
+                ContextCompat.getColor(this, R.color.orange)
+            bmi in (30.0..34.99) ->
+                ContextCompat.getColor(this, R.color.red)
+            bmi in (35.0..39.99) ->
+                ContextCompat.getColor(this, R.color.red)
+            else ->
+                ContextCompat.getColor(this, R.color.red)
+        }
+
+        return color
     }
 
     private fun openResultActivity(){
         val resultIntent = Intent(this, ResultActivity::class.java)
 
-//        resultIntent.putExtra("bmi", )
-//        resultIntent.putExtra("resultText", resultText)
+//        resultIntent.putExtra("bmi", resultBMI.text)
+//        resultIntent.putExtra("resultText", resultTextBMI.text)
 
         startActivity(resultIntent)
 
